3d4546ac767832a96ad34bc7b366f91e0cfdeb36
fazaimron27 <cincaumyself@gmail.com>
2021-01-10 00:29:50 +0700

    move helper

--- app/controllers/Post.php
+++ app/controllers/Post.php
@@ -3,16 +3,15 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
-use App\Helpers\Auth;
-use App\Helpers\Redirect;
+use App\Core\Helper;
 use App\Models\Post as PostModel;
 
 class Post extends Controller
 {
     public function __construct()
     {
-        if (!Auth::check()) {
-            Redirect::to('auth/login');
+        if (!Helper::auth()) {
+            Helper::redirect('auth/login');
         }
     }
 
@@ -33,7 +32,7 @@ class Post extends Controller
             'title' => $_POST['title'],
             'body' => $_POST['content']
         ]);
-        Redirect::to('post');
+        Helper::redirect('post');
     }
 
     public function edit($id)
@@ -49,13 +48,13 @@ class Post extends Controller
             'title' => $_POST['title'],
             'body' => $_POST['content']
         ]);
-        Redirect::to('post');
+        Helper::redirect('post');
     }
 
     public function destroy($id)
     {
         $user = PostModel::findOrFail($id);
         $user->delete();
-        Redirect::to('post');
+        Helper::redirect('post');
     }
 }
